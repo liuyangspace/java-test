1d8db48169a8ffd59278d95c7c49416d42b0a143
刘扬 <liuyang@huan.tv>
2018-04-18 14:50:53 +0800

    1，完善数据类型注释

--- src/language/data/number/Comparable.java
+++ src/language/data/number/Comparable.java
@@ -1,9 +1,13 @@
 package language.data.number;
 
 /**
+ * This interface imposes a total ordering on the objects of each class that
+ * implements it.  This ordering is referred to as the class's <i>natural
+ * ordering</i>, and the class's {@code compareTo} method is referred to as
+ * its <i>natural comparison method</i>.<p>
  *
  * @see java.lang.Comparable
  */
-public interface Comparable<T> extends java.lang.Comparable {
-    // public int compareTo(T o);
+public interface Comparable<T> {
+    public int compareTo(T o);
 }
