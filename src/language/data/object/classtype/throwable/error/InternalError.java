0f0f92e8b76f1544c0f805c4b70498edac6c2caa
刘扬 <liuyang@huan.tv>
2018-04-18 13:27:01 +0800

    1，完善异常注释

---
+++ src/language/data/object/classtype/throwable/error/InternalError.java
@@ -0,0 +1,19 @@
+package language.data.object.classtype.throwable.error;
+
+import java.lang.Error;
+
+/**
+ * @see java.lang.InternalError
+ */
+public class InternalError extends Error
+{
+    private static final long serialVersionUID = -9062593416125562365L;
+    public InternalError() {
+        super();
+    }
+    public InternalError(String message) { super(message); }
+    public InternalError(String message, Throwable cause) { super(message, cause); }
+    public InternalError(Throwable cause) {
+        super(cause);
+    }
+}
